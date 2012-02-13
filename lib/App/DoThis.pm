34207df14676e962b9602107e86f5b4392008842
SATOH, Koichi <r.sekia@gmail.com>
2012-02-13 18:26:31 +0900

    Introduce CPAN-style directory structure.

---
+++ lib/App/DoThis.pm
@@ -0,0 +1,32 @@
+package App::DoThis;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::DoThis -
+
+=head1 SYNOPSIS
+
+  use App::DoThis;
+
+=head1 DESCRIPTION
+
+App::DoThis is
+
+=head1 AUTHOR
+
+Koichi SATOH E<lt>sekia@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

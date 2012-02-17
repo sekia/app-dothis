c2b911c79ec1f9eaf01c97fa7fa3af10f83b6630
SATOH, Koichi <r.sekia@gmail.com>
2012-02-17 16:19:43 +0900

    Relocate codes to separate logic from interface.

--- lib/App/DoThis.pm
+++ lib/App/DoThis.pm
@@ -1,7 +1,74 @@
 package App::DoThis;
+
+use 5.014;
 use strict;
 use warnings;
+use AnyEvent;
+use Attribute::Constant;
+use Cocoa::Growl qw/:all/;
+use Exporter::Lite;
+
 our $VERSION = '0.01';
+our @EXPORT = qw/dothis
+                 growl_installed
+                 growl_running
+                 register_application/;
+
+my %NOTIFICATIONS : Constant(
+  DONE_SUCCESSFULLY => 'Done Successfully',
+  ERROR_OCCURED => 'Error Occured',
+  INTERRUPTED => 'Interrupted',
+);
+
+sub register_application {
+  my $app_name = shift;
+  growl_register(
+    app => $app_name,
+    notifications => [ values %NOTIFICATIONS ],
+  );
+}
+
+sub escape_command_line {
+  my $arg = shift;
+  if (index($arg, ' ') != -1) {
+    $arg =~ s/'/\\'/g;
+    return qq/'$arg'/;
+  }
+  return $arg;
+}
+
+sub dothis {
+  my ($cmd, @args) = @_;
+  my $command_line = join ' ', map { escape_command_line $_ } ($cmd, @args);
+  my $cv = AE::cv;
+
+  my $pid = fork;
+  die 'Forking failed\n' unless defined $pid;
+
+  if ($pid) {  # parent
+    my @watchers;
+    push @watchers, AE::child $pid => sub {
+      my (undef, $status) = @_;
+      undef @watchers;
+      my $result = $NOTIFICATIONS{
+        $status == 0 ? 'DONE_SUCCESSFULLY' : 'ERROR_OCCURED' };
+      $cv->send($result);
+    };
+    push @watchers, AE::signal 'INT' => sub {
+      undef @watchers;
+      $cv->send($NOTIFICATIONS{INTERRUPTED});
+    };
+
+    my $result = $cv->recv;
+    growl_notify(
+      name => $result,
+      title => $result,
+      description => $command_line,
+    );
+  } else {  # child
+    exec $cmd, @args or exit -1;
+  }
+}
 
 1;
 __END__
@@ -16,7 +83,7 @@ App::DoThis -
 
 =head1 DESCRIPTION
 
-App::DoThis is
+App::DoThis is a command line utility that excutes a command and notify its termination via Growl.
 
 =head1 AUTHOR
 
